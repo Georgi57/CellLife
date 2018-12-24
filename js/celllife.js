703414993f5066275b8348469c41a54cc4e76cf7
GEORGI-ASPIREV1\georgi <georgi.olentsenko@gmail.com>
2018-12-24 17:25:50 +0100

    Added cell type, identified by color

--- js/celllife.js
+++ js/celllife.js
@@ -44,10 +44,10 @@ function create_new_world()
 	world = world_view.createImageData(world_height,world_width);
 	for (var i=0;i<world.data.length;i+=4)
 	{
-		world.data[i+0]=239;
-		world.data[i+1]=224;
-		world.data[i+2]=199;
-		world.data[i+3]=255;
+		world.data[i+0]=50; // red
+		world.data[i+1]=50; // green
+		world.data[i+2]=255; // blue
+		world.data[i+3]=200;
 	}
 	world_view.putImageData(world,0,0);
 }
@@ -55,18 +55,23 @@ function create_new_world()
 function create_life()
 {
 	var i;
-	for (i = 0; i < 50; i++) {
+	for (i = 0; i < 200; i++) {
 		
 		// Cell parameters
 		var new_cell = {
 			number: cell_total_number, 	// Cell number
 			x: 0,		// location x
-			y: 0		// location y
+			y: 0,		// location y
+			type: 'g'
 		};
 		
 		cell_total_number += 1;
 		new_cell.x = Math.floor((Math.random() * world_width));
 		new_cell.y = Math.floor((Math.random() * world_height));
+		
+		var type = Math.floor((Math.random() * 10));
+		if (type==0) new_cell.type = 'r';
+		
 		cells.push(new_cell);
 	}
 }
@@ -113,8 +118,18 @@ function live()
 	
 	for (i = 0; i < cells.length; i++) {
 		world_location = (cells[i].y*world_width + cells[i].x)*4;
-		world.data[world_location+0]=0;
-		world.data[world_location+1]=0;
+		
+		if (cells[i].type=='r')
+		{
+			world.data[world_location+0]=150;
+			world.data[world_location+1]=0;
+		}
+		else
+		{
+			world.data[world_location+0]=0;
+			world.data[world_location+1]=150;
+		}
+		
 		world.data[world_location+2]=0;
 	}
 	world_view.putImageData(world,0,0);
